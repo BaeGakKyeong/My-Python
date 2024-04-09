1cba2a9c082cba6dd113a38b5b90ebaa48fea891
BaeGakKyeong <bggcarl@gmail.com>
2024-04-09 20:09:12 +0900

    add comment

--- PE/circle_with_variable.py
+++ PE/circle_with_variable.py
@@ -1,8 +1,9 @@
 #변수를 이용해서 원의 면적, 둘레 구하기
 
-radius = 6.0
+radius = 6.0    #원의 반지름을 radius라는 변수로 정의함
 Pi = 3.14
 
+#원의 반지름과 면적, 둘레 각각 출력
 print('원의 반지름', radius)
-print('원의 면적', Pi * radius ** 2)
-print('원의 둘레', Pi * radius * 2)
+print('원의 면적', Pi * radius ** 2)    #원의 면적을 구하는 식
+print('원의 둘레', Pi * radius * 2)     #원의 둘레를 구하는 식

26d7b17fadeddfa3a1ac86c113f21e89e10d2529
BaeGakKyeong <bggcarl@gmail.com>
2024-04-16 13:43:09 +0900

    convert inputted celsius to fahrnheit output

---
+++ PE/cel2fah.py
@@ -0,0 +1,27 @@
+#P107 Q.2.9 섭씨 온도를 입력받아 화씨 온도로 변환하는 프로그램
+
+celsius = float(input('섭씨 온도를 입력하시오 : '))
+fahrenheit = (9/5) * celsius + 32
+
+print('섭씨 \t 화씨')
+print(celsius, '\t', fahrenheit)
+
+celsius += 10
+fahrenheit = (9/5) * celsius + 32
+print(celsius, '\t', fahrenheit)
+
+celsius += 10
+fahrenheit = (9/5) * celsius + 32
+print(celsius, '\t', fahrenheit)
+
+celsius += 10
+fahrenheit = (9/5) * celsius + 32
+print(celsius, '\t', fahrenheit)
+
+celsius += 10
+fahrenheit = (9/5) * celsius + 32
+print(celsius, '\t', fahrenheit)
+
+celsius += 10
+fahrenheit = (9/5) * celsius + 32
+print(celsius, '\t', fahrenheit)

18499c4719fac69bd486df3787a198e56adaf58c
BaeGakKyeong <bggcarl@gmail.com>
2024-04-19 14:31:06 +0900

    leap year v0.2

--- PE/if_leap_year_test.py
+++ PE/if_leap_year_test.py
@@ -4,17 +4,12 @@ year = int(input('년도를 입력하시오 :'))
 
 if year % 4 == 0 :
     
-    if year % 100 == 0 :
-        
-        if year % 400 == 0 :
-            print('윤년입니다.')
-            
-        else :
-            print('윤년이 아닙니다.')
-        
-    else :
+    if (year % 100 != 0) or (year % 400 == 0) :
         print('윤년입니다.')
     
+    else :
+        print('윤년이 아닙니다.')
+        
 else :
     print('윤년이 아닙니다.')
     

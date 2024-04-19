7116eb0c24580a954e1390ea59d3964923b031d0
BaeGakKyeong <bggcarl@gmail.com>
2024-04-19 17:42:05 +0900

    grade v1.0

--- PE/if_grade.py
+++ PE/if_grade.py
@@ -5,18 +5,16 @@ score = int(input('100 이하의 자연수를 입력하시오 :'))
 if score >= 90 :
     grade = 'A'
 
+elif score >= 80 :
+    grade = 'B'
+    
+elif score >= 70 :
+    grade = 'C'
+    
+elif score >= 60 :
+    grade = 'D'
+
 else :
-    if score >= 80 :
-        grade = 'B'
-    else:
-        if score >= 70 :
-            grade = 'C'
-            
-        else:
-            if score >= 60 :
-                grade = 'D'
-                
-            else :
-                grade = 'F'
+    grade = 'F'
     
 print(score, '점은', grade, '입니다.')

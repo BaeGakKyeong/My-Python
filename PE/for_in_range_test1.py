420055b2afade43621afe108b18972889cf95e1a
BaeGakKyeong <bggcarl@gmail.com>
2024-04-21 19:00:53 +0900

    keyword 'end'

--- PE/for_in_range_test1.py
+++ PE/for_in_range_test1.py
@@ -3,4 +3,8 @@
 for i in range(5) :
     print(i, end = '')     
     #키워드 end는 print함수 출력 후 내보낼 문자를 지정하기 위한것으로, end를 사용해 지정하지 않으면 줄바꿈 문자(\n)가 출력된다
+print()
+
+for i in range(5) :
+    print(i, end = ' ')
     

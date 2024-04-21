1fb6b182dc6861d077e0e67d9a9d09add3179d3c
BaeGakKyeong <bggcarl@gmail.com>
2024-04-21 21:34:02 +0900

    v0.2

--- PE/for_sum_ex1.py
+++ PE/for_sum_ex1.py
@@ -4,6 +4,8 @@ x = int(input('자연수를 입력하시오 : '))
 s = 0   #변수 초기화를 반드시 해 주어야 한다
 
 for i in range(1, x + 1) :  #range 함수에 변수와 수식을 넣어도 작동한다.
+    print(s, '+', i, '=', end = ' ')
     s += i
+    print(s)
     
-print(s)
+print('1부터', x, '까지의 합은', s, '입니다.')

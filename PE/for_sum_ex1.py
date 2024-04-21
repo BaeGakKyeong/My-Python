bc9da31c5832d3078331f68a23ef476cce5d25cc
BaeGakKyeong <bggcarl@gmail.com>
2024-04-21 21:25:44 +0900

    sumery from one to (input)

--- PE/for_sum_ex1.py
+++ PE/for_sum_ex1.py
@@ -1,8 +1,9 @@
 #연속적인 값의 생성과 누적 덧셈
 
+x = int(input('자연수를 입력하시오 : '))
 s = 0   #변수 초기화를 반드시 해 주어야 한다
 
-for i in range(1, 11) :
+for i in range(1, x + 1) :  #range 함수에 변수와 수식을 넣어도 작동한다.
     s += i
     
 print(s)

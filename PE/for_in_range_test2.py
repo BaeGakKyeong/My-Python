aee4e7848b6f7fae1507fb421f2c983ba0932ced
BaeGakKyeong <bggcarl@gmail.com>
2024-04-21 21:18:50 +0900

    fungtion of range()

---
+++ PE/for_in_range_test2.py
@@ -0,0 +1,17 @@
+#range함수의 다양한 활용
+
+for i in range(5) :
+    print(i, end = ' ')
+print()
+
+for i in range(1, 5) :
+    print(i, end = ' ')
+print()
+
+for i in range(0, -10, -2) :
+    print(i, end = ' ')
+print()
+
+for i in range(-10, 0, 2) :
+    print(i, end = '')
+    

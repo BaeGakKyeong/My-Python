4daafc2b0ee82d0849f68e3682f90b70f0624375
BaeGakKyeong <bggcarl@gmail.com>
2024-04-24 18:04:20 +0900

    printing patterns

--- PE/double_for_pattern.py
+++ PE/double_for_pattern.py
@@ -13,4 +13,23 @@ for i in range(7) :
         st += ' '
     print(st + '#')
     
-#내 주관으로는, 내 코드의 가독성이 더 좋다고 생각한다. 내 풀이에는 print()문이, 교재의 풀이에는 변수가 각각 하나씩 많기 때문에 효윯성은 같다.
+#내 풀이에는 print()문이, 교재의 풀이에는 변수가 각각 하나씩 많기 때문에 효윯성은 같다.
+
+#교재의 다른 풀이
+for i in range(7) :
+    print(' ' * i + '#')    #문자열과 변수는 연산할 수 있다. 
+
+#for문도 하나고, 변수도 하나 뿐이기 때문에 코드의 효율성과 가독성이 모두 좋다. 
+
+print()
+print()
+
+#LAB 3-9 : 패턴 출력 응용
+for i in range(6, -1, -1) :
+    for j in range(0, i) :
+        print(end = ' ')
+    print('#')
+print()
+
+for i in range(6, -1, -1) :
+    print(' ' * i + '#')

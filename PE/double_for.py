b908838485ce7df84dfbf1d8c43cf87097664082
BaeGakKyeong <bggcarl@gmail.com>
2024-04-24 17:12:45 +0900

    multiplication table v0.2

--- PE/double_for.py
+++ PE/double_for.py
@@ -1,8 +1,14 @@
 #이중 for문을 이용한 구구단의 출력
 
-for x in range(2, 10) :
-    for y in range(1, 10) :
-        print(x, '*', y, '=', x * y)
+for x in range(1, 10) :
+    for y in range(2, 6) :
+        print(y, '*', x, '=', x * y, end = '\t')
+    print()
+print()
+
+for x in range(1, 10) :
+    for y in range(6, 10) :
+        print(y, '*', x, '=', x * y, end = '\t')
     print()
     
         

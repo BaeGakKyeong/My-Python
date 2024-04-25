fe7eaeec36d71c3927deb31ce7f6cbc634e946f7
BaeGakKyeong <bggcarl@gmail.com>
2024-04-25 19:26:10 +0900

    printing star patterns

--- PE/double_for_patter2.py
+++ PE/double_for_patter2.py
@@ -4,3 +4,12 @@ j = 4
 for i in range(1, 10, 2) :
     print(' ' * j + '*' * i)
     j -= 1
+    
+#이중 for문을 이용한 별찍기
+
+st = '*'
+for i in range(5) :     #[0, 1, 2, 3, 4]
+    for j in range(4-i, 0, -1) :
+        print(end = ' ')
+    print(st)
+    st += '**'

216a27c434f12652cdde29604d81741f1c642a45
BaeGakKyeong <bggcarl@gmail.com>
2024-05-01 17:23:27 +0900

    parameter and arguement

---
+++ PE/print_star_param.py
@@ -0,0 +1,7 @@
+#매개변수를 가진 별표 출력 함수와 인자를 이용한 호출
+
+def print_star(n):  #매개변수(parameter) n
+    for i in range(n) :
+        print(i, '***********************')
+        
+print_star(4)   #인자(arguement) 4
